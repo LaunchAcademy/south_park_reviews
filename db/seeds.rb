4e0871112a19f1e6ba9009c64f97eaf2b9479c59
Eric Kelly <heroiceric@gmail.com>
2014-09-23 17:08:17 -0400

    Revert "Add episodes"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'csv'
-require 'episode_importer'
-EpisodeImporter.new('episode_data.csv').import
