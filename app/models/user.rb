fcf8e4fad7df432c58d5e0f97558f193c7f11acb
Andrew <akcrono@gmail.com>
2014-09-29 09:47:49 -0400

    reverted AvatarUploader change; test failed

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mount_uploader :profile_image, ProfileImageUploader
+  mount_uploader :profile_image, AvatarUploader
 
   attr_accessor :login
 
