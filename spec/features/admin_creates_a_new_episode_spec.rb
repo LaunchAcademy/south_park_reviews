c1e62bb875339ab709a988b8e044134ed6348bbc
Andrew <akcrono@gmail.com>
2014-09-30 16:19:54 -0400

    added test for episode synopsis markdown

--- spec/features/admin_creates_a_new_episode_spec.rb
+++ spec/features/admin_creates_a_new_episode_spec.rb
@@ -23,4 +23,17 @@ feature 'User creates a new episode as Admin' do
 
     expect(page).to have_content 'Invalid entry'
   end
+
+  scenario 'Admin creates an episode with markdown' do
+    admin = FactoryGirl.create(:admin)
+    sign_in_as(admin)
+    visit new_episode_path
+    fill_in 'episode_title', with: 'Markdown, woo'
+    select '22', from: 'episode_season'
+    select '23', from: 'episode_episode_number'
+    fill_in 'Synopsis', with: "**bold** *italics*"
+    click_button 'Create Episode'
+
+    expect(page).to_not have_content "**bold** *italics*"
+  end
 end
