8d0202c1e9b4c5e6f297ebdd63aceca5deb5b387
John Allen <john_allen3@student.uml.edu>
2014-09-30 18:37:20 -0400

    Made Travis CI Happy(hopefully)

--- spec/features/user_searches_for_episode_spec.rb
+++ spec/features/user_searches_for_episode_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 feature 'User searches for an episode' do
+  load "#{Rails.root}/db/seeds.rb"
   scenario 'User searches for poo' do
     visit root_path
     fill_in 'search', with: 'poo'
