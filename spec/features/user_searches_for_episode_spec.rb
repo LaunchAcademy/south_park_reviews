ab54194f460495714ee190ce492b28c574ff38b8
John Allen <john_allen3@student.uml.edu>
2014-10-01 10:40:11 -0400

    Added a few more tests

--- spec/features/user_searches_for_episode_spec.rb
+++ spec/features/user_searches_for_episode_spec.rb
@@ -9,4 +9,11 @@ feature 'User searches for an episode' do
     expect(page).to have_content 'the Christmas Poo'
     expect(page).to_not have_content 'Unaired pilot (28 min)'
   end
+
+  scenario 'User clicks on Search(Retrieving all episodes)' do
+    visit root_path
+    click_button 'Search'
+
+    expect(page).to have_content 'S1E1'
+  end
 end
--- spec/mailers/episode_mailer_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe EpisodeMailer, :type => :mailer do
-  
-end
